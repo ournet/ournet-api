f171a6e9a9fa62c1228505ce0626a920bff048f2
Dumitru C <canteadumitru@gmail.com>
2022-03-09 21:49:00 +0200

    horoscopes-data@0.1.2

--- src/graphql/horoscopes/resolvers.ts
+++ src/graphql/horoscopes/resolvers.ts
@@ -1,27 +1,47 @@
-
-import { Context } from '../../context';
-import { GenerateReportUseCase, HoroscopeSign, Report } from '@ournet/horoscopes-domain';
+import { Context } from "../../context";
+import {
+  GenerateReportUseCase,
+  HoroscopeSign,
+  Report
+} from "@ournet/horoscopes-domain";
 
 export default {
-    Query: {
-        horoscopes_reportById: (_: any, args: { id: string }, context: Context) => {
-            return context.data.horoReportRep.getById(args.id);
-        },
-        horoscopes_reportsByIds: (_: any, args: { ids: string[] }, context: Context) => {
-            return context.data.horoReportRep.getByIds(args.ids);
-        },
-        horoscopes_phraseById: (_: any, args: { id: string }, context: Context) => {
-            return context.data.horoPhraseRep.getById(args.id);
-        },
-        horoscopes_generateReports: async (_: any, args: { params: { lang: string, period: string } }, context: Context) => {
-            const list: Report[] = []
-            const generate = new GenerateReportUseCase(context.data.horoPhraseRep, context.data.horoReportRep);
-            for (const sign of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] as HoroscopeSign[]) {
-                const report = await generate.execute({ lang: args.params.lang, period: args.params.period, sign });
-                list.push(report);
-            }
+  Query: {
+    horoscopes_reportById: (_: any, args: { id: string }, context: Context) => {
+      return context.data.horoReportRep.getById(args.id);
+    },
+    horoscopes_reportsByIds: (
+      _: any,
+      args: { ids: string[] },
+      context: Context
+    ) => {
+      return context.data.horoReportRep.getByIds(args.ids);
+    },
+    horoscopes_phraseById: (_: any, args: { id: string }, context: Context) => {
+      return context.data.horoPhraseRep.getById(args.id);
+    },
+    horoscopes_generateReports: async (
+      _: any,
+      args: { params: { lang: string; period: string } },
+      context: Context
+    ) => {
+      const list: Report[] = [];
+      const generate = new GenerateReportUseCase(
+        context.data.horoPhraseRep,
+        context.data.horoReportRep
+      );
+      for (const sign of [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+      ] as HoroscopeSign[]) {
+        const report = await generate.execute({
+          lang: args.params.lang,
+          period: args.params.period,
+          sign
+        });
+        list.push(report);
+      }
 
-            return list;
-        }
+      return list;
     }
-}
+  }
+};
