2f31e30daf93c19532fee9cea637c21120a5c4fb
Dumitru C <canteadumitru@gmail.com>
2023-02-20 18:14:12 +0200

    Update auth.ts

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,12 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import { logger } from "../logger";
 
+const getToken = (req: Request): string | string[] | null =>
+  (req.query["api_token"]?.length
+    ? (req.query["api_token"] as never)
+    : (null as never)) ||
+  req.headers.authorization ||
+  null;
+
 export function auth(req: Request, res: Response, next: NextFunction) {
   let hasData = false;
-  if (req.headers.authorization) {
-    const parts = Array.isArray(req.headers.authorization)
-      ? req.headers.authorization
-      : (<string>req.headers.authorization).split(" ");
+  const token = getToken(req);
+  if (token) {
+    const parts = Array.isArray(token) ? token : (<string>token).split(" ");
     if (parts.length === 2) {
       hasData = true;
       if (parts[1] === process.env.OURNET_API_KEY) {
@@ -22,10 +28,9 @@ export function auth(req: Request, res: Response, next: NextFunction) {
 }
 
 export function isAuthenticated(req: Request) {
-  if (req.headers.authorization) {
-    const parts = Array.isArray(req.headers.authorization)
-      ? req.headers.authorization
-      : (<string>req.headers.authorization).split(" ");
+  const token = getToken(req);
+  if (token) {
+    const parts = Array.isArray(token) ? token : (<string>token).split(" ");
     if (parts.length === 2) {
       if (parts[1] === process.env.OURNET_API_KEY) {
         return true;
